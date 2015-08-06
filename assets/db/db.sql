3cced71fcf0d845bc1060ded4a787da0279962ad
unknown <talhauddin@TalhaUddin.folio3pk.com>
2015-08-06 19:05:05 +0500

    Db Backup

--- assets/db/db.sql
+++ assets/db/db.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Enterprise - MySQL GUI v7.02 
-MySQL - 5.6.12-log : Database - club
+MySQL - 5.6.17 : Database - club
 *********************************************************************
 */
 
@@ -8,16 +8,9 @@ MySQL - 5.6.12-log : Database - club
 
 /*!40101 SET SQL_MODE=''*/;
 
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`club` /*!40100 DEFAULT CHARACTER SET latin1 */;
-
-USE `club`;
-
 /*Table structure for table `content` */
-
-DROP TABLE IF EXISTS `content`;
 
 CREATE TABLE `content` (
   `content_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -25,18 +18,14 @@ CREATE TABLE `content` (
   `title` varchar(255) DEFAULT NULL,
   `description` text,
   `detail_description` text,
-  `date` datetime DEFAULT NULL,
+  `date` date DEFAULT NULL,
   `is_active` tinyint(1) DEFAULT '1' COMMENT '0=inactive, 1=active',
   PRIMARY KEY (`content_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*Data for the table `content` */
 
-insert  into `content`(`content_id`,`content_type_id`,`title`,`description`,`detail_description`,`date`,`is_active`) values (1,1,'event 1','farhan',NULL,'0000-00-00 00:00:00',1),(2,1,'aasdfsaf','asfsdfasfasf',NULL,'2015-08-21 00:00:00',1);
-
 /*Table structure for table `content_type` */
-
-DROP TABLE IF EXISTS `content_type`;
 
 CREATE TABLE `content_type` (
   `content_type_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -49,8 +38,6 @@ CREATE TABLE `content_type` (
 insert  into `content_type`(`content_type_id`,`content`) values (1,'events'),(2,'courses');
 
 /*Table structure for table `devices` */
-
-DROP TABLE IF EXISTS `devices`;
 
 CREATE TABLE `devices` (
   `device_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -68,8 +55,6 @@ insert  into `devices`(`device_id`,`user_id`,`uid`,`type`,`lang`,`token`) values
 
 /*Table structure for table `image` */
 
-DROP TABLE IF EXISTS `image`;
-
 CREATE TABLE `image` (
   `image_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
@@ -83,8 +68,6 @@ CREATE TABLE `image` (
 
 /*Table structure for table `page` */
 
-DROP TABLE IF EXISTS `page`;
-
 CREATE TABLE `page` (
   `page_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `key` varchar(255) DEFAULT NULL,
@@ -96,8 +79,6 @@ CREATE TABLE `page` (
 /*Data for the table `page` */
 
 /*Table structure for table `users` */
-
-DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `users` (
   `user_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -114,4 +95,3 @@ CREATE TABLE `users` (
 insert  into `users`(`user_id`,`username`,`password`,`is_active`,`verified`,`is_admin`) values (82,'farhan.bashir2002@gmail.com','click123',1,1,0),(83,'admin@club.com','click123',1,1,1);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
