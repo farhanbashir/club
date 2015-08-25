1824574720e4a05126cb92e2fdedb40167039515
talhauddin <talha.udin@gmail.com>
2015-08-26 02:40:37 +0500

    no message

--- application/views/login.php
+++ application/views/login.php
@@ -4,10 +4,10 @@
         <meta charset="UTF-8">
         <title>Alef</title>
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-        <link href="<?php echo asset_css('bootstrap.min.css');?>" rel="stylesheet" type="text/css" />
-        <link href="<?php echo asset_css('font-awesome.min.css');?>" rel="stylesheet" type="text/css" />
+        <link href="<?php echo asset_css('bootstrap.min.css'); ?>" rel="stylesheet" type="text/css" />
+        <link href="<?php echo asset_css('font-awesome.min.css'); ?>" rel="stylesheet" type="text/css" />
         <!-- Theme style -->
-        <link href="<?php echo asset_css('AdminLTE.css');?>" rel="stylesheet" type="text/css" />
+        <link href="<?php echo asset_css('AdminLTE.css'); ?>" rel="stylesheet" type="text/css" />
 
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -19,16 +19,17 @@
     <body class="bg-black">
 
         <div class="form-box" id="login-box">
+            <img src="http://localhost/club/assets/img/logo.png" class="logo icon"style="margin: -29px 0px 30px 109px;F" alt="User Image">
+            <br/>
             <div class="header">Sign In</div>
-            <form action="<?php echo base_url();?>index.php/auth/check_database" method="post">
+            <form action="<?php echo base_url(); ?>index.php/auth/check_database" method="post">
                 <div class="body bg-gray">
                     <?php
-                    if(isset($error))
-                    {
+                    if (isset($error)) {
                         echo '<div class="callout callout-danger">
-                                '.$error.'
+                                ' . $error . '
                             </div>';
-                    }    
+                    }
                     ?>
                     <div class="form-group">
                         <input type="text" name="username" class="form-control" placeholder="User ID"/>
@@ -42,10 +43,10 @@
                 </div>
                 <div class="footer">                                                               
                     <button type="submit" class="btn bg-olive btn-block">Sign me in</button>  
-                    
-                    <!-- <p><a href="#">I forgot my password</a></p>
-                    
-                    <a href="register.html" class="text-center">Register a new membership</a> -->
+
+<!-- <p><a href="#">I forgot my password</a></p>
+
+<a href="register.html" class="text-center">Register a new membership</a> -->
                 </div>
             </form>
 
@@ -59,8 +60,8 @@
             </div> -->
         </div>
 
-        <script src="<?php echo asset_js('jquery.min.js');?>"></script>
-        <script src="<?php echo asset_js('bootstrap.min.js');?>" type="text/javascript"></script>
+        <script src="<?php echo asset_js('jquery.min.js'); ?>"></script>
+        <script src="<?php echo asset_js('bootstrap.min.js'); ?>" type="text/javascript"></script>
 
     </body>
 </html>
